bdb4781468c6c25bcb8babbf24819699db417cd6
vmametmru <vmamet@mamba-online.com>
2025-12-26 06:27:28 +0400

    Create contact.ts

---
+++ src/lib/contact.ts
@@ -0,0 +1,7 @@
+export const CONTACT = {
+  phones: [
+    { display: "+230 52544544", tel: "+23052544544" },
+    { display: "+230 54221025", tel: "+23054221025" },
+  ],
+  combined: "+230 52544544 / +230 54221025",
+} as const;
